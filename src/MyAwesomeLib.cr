b20358dd384670993811384b04a28988329b71f0
Çağatay ÇALI <me@calicagatay.me>
2016-05-06 06:41:10 +0300

    Achivement

--- src/MyAwesomeLib.cr
+++ src/MyAwesomeLib.cr
@@ -1,15 +1,7 @@
-def thrice
-  puts "Before 1"
+def twice
   yield 1
-  puts "Before 2"
   yield 2
-  puts "Before 3"
-  yield 3
-  puts "After 3"
 end
 
-thrice do |i|
-  if i == 2
-    break
-  end
-end
+twice { |i| puts i + 1 } #=> 3
+twice { |i| break puts "hello" } #=> "hello"
