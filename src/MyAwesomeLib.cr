5ceb0eb5030a219c847033fe8824819bd30e7e13
Çağatay ÇALI <me@calicagatay.me>
2016-05-06 06:07:28 +0300

    Def learned

--- src/MyAwesomeLib.cr
+++ src/MyAwesomeLib.cr
@@ -1,36 +1,16 @@
-class Person
+def twice
+  yield
+  yield
+end
 
-  @name : String
-  @age : Int32
-
-  def initialize(@name,@age)
-
-  end
-
-  def name
-    @name
-  end
-
-  def age
-    @age
-  end
-
-
+def test
+  yield
+  yield
+  yield
+  puts "Test"
 end
 
 
-cagatay = Person.new "Cagatay", 34
-baris = Person.new "Baris", 31
-
-cagatay.name #=> "Cagatay"
-cagatay.age #=> 34
-
-baris.name #=> "Baris"
-baris.age #=> "31"
-
-
-puts cagatay.name
-puts cagatay.age
-
-puts baris.name
-puts baris.age
+test do
+  puts "Hello!"
+end
